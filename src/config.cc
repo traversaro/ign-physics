f79843b2b842ffbd7a7f69d206e4af34b52b9006
Silvio Traversaro <silvio.traversaro@iit.it>
2022-11-22 00:06:31 +0100

    Fix for binary relocatability

---
+++ src/config.cc
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <ignition/physics/config.hh>
+
+#include <filesystem>
+
+namespace ignition
+{
+  namespace physics
+  {
+    std::string getEngineInstallDir()
+    {
+      std::filesystem::path engine_relative_install_dir(IGNITION_PHYSICS_ENGINE_RELATIVE_INSTALL_DIR);
+      std::filesystem::path install_prefix(getInstallPrefix());
+      std::filesystem::path engine_install_dir = install_prefix / engine_relative_install_dir;
+      return engine_install_dir.string();
+    }
+  }
+}
